94b12570d372e83f4b128d8e2689e3e72c0f9758
Thiago Cerutti <thiagocerutti@gmail.com>
2023-05-01 12:31:42 -0300

    chore: change google sheets authentication

--- src/services/gsheets/google-sheets.js
+++ src/services/gsheets/google-sheets.js
@@ -1,14 +1,16 @@
-const { GoogleAuth } = require("google-auth-library");
+const { GoogleAuth, auth } = require("google-auth-library");
 const { google } = require("googleapis");
 
-process.env["GOOGLE_APPLICATION_CREDENTIALS"] =
-  "./src/services/gsheets/google_application_credentials.json";
+const envCredentials = JSON.parse(
+  Buffer.from(process.env.GAC, "base64").toString("utf8")
+);
+
+const client = auth.fromJSON(envCredentials);
+client.scopes = process.env.SCOPES;
 
 const service = google.sheets({
   version: process.env.API_VERSION,
-  auth: new GoogleAuth({
-    scopes: process.env.SCOPES,
-  }),
+  auth: client,
 });
 
 module.exports = service;
