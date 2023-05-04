d703fc853d98f1ba941e9a8940ce6f4213298510
Thiago Cerutti <thiagocerutti@gmail.com>
2023-05-03 22:53:40 -0300

    formata informações do educando para card

--- src/components/CardEducando.js
+++ src/components/CardEducando.js
@@ -1,11 +1,27 @@
+import Card from "react-bootstrap/Card";
+
 const CardEducando = ({ educando }) => {
   return (
-    <div>
-      <h2>{educando.nomeCompleto}</h2>
-      <p>Unidade: {educando.unidade}</p>
-      <p>Etapa: {educando.etapa}</p>
-      <p>Trilha: {educando.trilha}</p>
-    </div>
+    <>
+      <Card
+        style={{ width: "18rem", margin: "10px" }}
+        className="card-educando"
+        bg="light"
+      >
+        <Card.Header>
+          <Card.Title>{educando.nomeCompleto}</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <strong>Unidade:</strong> {educando.unidade}
+          </Card.Text>
+          <Card.Text>
+            <strong>Etapa:</strong> {educando.etapa} ({educando.trilha})
+          </Card.Text>
+          <Card.Link href="#">Ver perfil completo</Card.Link>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
